d856a6d3f6ebc6ed6289d357e6773095283a1ef6
Bucci <kibuchikahuthu@gmail.com>
2025-07-02 23:35:39 +0300

    Added conditional statements to the app to select between happy, sad and tired. Each mood prints a corresponding poem to go with the mood.

--- main.py
+++ main.py
@@ -10,12 +10,38 @@ def response(mood):
     # Responds with a message based on their input
     print("\nThanks for sharing. Here is something for you.")
 
-    poem = """
-If all trees die, if all trees go,  
-Where will our beautiful flowers grow?  
-No shade, no shelter,  
-What a shame!
-"""
+    if mood.lower() == "happy":
+        poem = """
+        Sunshine laughter fills the air,  
+        Your joy is something bright and rare.  
+        Hold on to that spark so true,  
+        And let your happiness carry you.
+        """
+
+    elif mood.lower() == "sad":
+        poem = """
+        Rain may fall, the skies may gray,  
+        But storms will always pass away.  
+        Hold on gently, take your time,  
+        Even sadness has its rhyme.
+        """
+
+    elif mood.lower() == "tired":
+        poem ="""
+        Rest your mind, and close your eyes,  
+        You've carried weight, you've reached the skies.  
+        Take a break and just be still,  
+        Your strength will rise; it always will.
+        """
+        
+    else:
+        poem = """
+        If all trees die, if all trees go,  
+        Where will our beautiful flowers grow?  
+        No shade, no shelter,  
+        What a shame!
+        """
+
     print(poem)
 
 def main():
